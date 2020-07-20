162689f89679d37338ddd60f58e723e27865e5ba
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2020-07-20 12:08:30 +0530

    ordering flags

--- main.go
+++ main.go
@@ -1,42 +1,48 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
+	"sort"
 
 	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	var language string
-
 	app := &cli.App{
 		Flags: []cli.Flag{
 			&cli.StringFlag{
-				Name:        "lang",
-				Value:       "english",
-				Usage:       "language for the greeting",
-				Destination: &language,
+				Name:  "lang, l",
+				Value: "english",
+				Usage: "Language for the greeting",
 			},
 			&cli.StringFlag{
 				Name:  "config, c",
 				Usage: "Load configuration from `FILE`",
 			},
 		},
-		Action: func(c *cli.Context) error {
-			name := "someone"
-			if c.NArg() > 0 {
-				name = c.Args().Get(0)
-			}
-			if language == "spanish" {
-				fmt.Println("Hola", name)
-			} else {
-				fmt.Println("Hello", name)
-			}
-			return nil
+		Commands: []*cli.Command{
+			{
+				Name:    "complete",
+				Aliases: []string{"c"},
+				Usage:   "complete a task on the list",
+				Action: func(c *cli.Context) error {
+					return nil
+				},
+			},
+			{
+				Name:    "add",
+				Aliases: []string{"a"},
+				Usage:   "add a task to the list",
+				Action: func(c *cli.Context) error {
+					return nil
+				},
+			},
 		},
 	}
+
+	sort.Sort(cli.FlagsByName(app.Flags))
+	sort.Sort(cli.CommandsByName(app.Commands))
 
 	err := app.Run(os.Args)
 	if err != nil {
